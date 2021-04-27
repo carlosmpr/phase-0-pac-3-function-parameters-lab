6b554e80bb749b8e4ff05562472e7c0a74fdb34a
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-04-27 07:10:53 -0400

    Completed

---
+++ index.js
@@ -0,0 +1,3 @@
+const introduction = (name) => `Hi, my name is ${name}.`
+const introductionWithLanguage = (name, language) => `Hi, my name is ${name} and I am learning to program in ${language}.`
+const introductionWithLanguageOptional = (name, language="JavaScript") => `Hi, my name is ${name} and I am learning to program in ${language}.`
